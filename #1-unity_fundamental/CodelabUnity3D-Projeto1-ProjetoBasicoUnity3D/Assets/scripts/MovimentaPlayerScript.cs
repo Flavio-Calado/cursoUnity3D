64ff2a5d563962d6d769f0acec903f8dfd454608
Flavio <flavio.calado@prof.una.br>
2017-04-10 17:30:31 -0300

    Curso 10/04/17

---
+++ #1-unity_fundamental/CodelabUnity3D-Projeto1-ProjetoBasicoUnity3D/Assets/scripts/MovimentaPlayerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovimentaPlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // pega a posição atual do player no jogo
+        Vector3 posicao = transform.position;
+        float movimentoLateral = Input.GetAxis("Horizontal");
+        float movimentoVertical = Input.GetAxis("Vertical");
+        // faz a multiplicação do movimento pelo tempo entre os frames
+		movimentoLateral = movimentoLateral * Time.deltaTime;
+		movimentoVertical = movimentoVertical * Time.deltaTime;
+        // aplica os movimentos sobre o player
+        transform.Translate(movimentoLateral, 0f, movimentoVertical);
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,1 @@
+Versão Develop
